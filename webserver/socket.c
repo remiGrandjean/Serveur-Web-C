34d9fef909d77e4dda6d55cd856ed9777cc906f5
Remi Grandjean <remi1360@voila.fr>
2015-01-26 10:58:52 +0100

    Code de base

---
+++ webserver/socket.c
@@ -0,0 +1,42 @@
+#include "serveur.h"
+
+int creer_serveur(int port){
+  int socket_server;
+  int socket_client;
+
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(8000);
+  saddr.sin_addr.s_addr = INADDR_ANY;
+ 
+  socket_server=socket(AF_INET,SOCK_STREAM,0);
+  socket_client=accept(socket_server,NULL,NULL);
+
+ 
+
+  if(socket_server==-1){
+    perror("socket serveur");
+    /*Traitement de l'erreur*/
+  }
+  /*Utilisation de la socket */
+
+  if(socket_client==-1){
+    perror("accept");
+    /* Traitement de l'erreur*/
+  }
+  const char * message_bienvenue = "Bonjour , bienvenue sur mon serveur \n";
+  write (socket_client , message_bienvenue , strlen(message_bienvenue));
+
+
+  if(bind(socket_server,(struct sockaddr *)&saddr, sizeof(saddr)) == -1){
+    perror("bind socket_server");
+    /*Traitement de l'erreur*/
+  }
+
+  if(listen(socket_server,10)==-1){
+    perror("listen socket_server");
+    /*Traitement de l'erreur*/
+  }
+  
+  
+}
---
+++ webserver/socket.h
@@ -0,0 +1,13 @@
+#ifndef __SOCKET_H__
+#define __SOCKET_H__
+
+
+
+int creer_serveur (int port);
+
+#endif
+
+
+
+
+
